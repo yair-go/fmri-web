6e764bbb52f7e0f589c0996a3f90da08f6af55c5
boaz88 <boaz88@e7c778be-5e33-bf5c-8bb5-02dc48ffe900>
2010-05-11 06:22:13 +0000

    better main - allow passing parameters from the command line

--- trunk/fmri-web --username boaz88/clique_algo/Clique_Tester.java
+++ trunk/fmri-web --username boaz88/clique_algo/Clique_Tester.java
@@ -6,34 +6,62 @@ import java.io.PrintWriter;
 import java.util.Vector;
 
 public class Clique_Tester {
-	public static int Q_size = 9;
-	public static double TH = 0.7;
+	public static int minQ = 5, maxQ=7;
+	public static double TH = 0.8;
 	public static String in_file = "test1.csv";
+	public static String out_file = in_file+"_out.txt";
 	
 	
 	public static void main(String[] args) {
-		Graph G = new Graph(in_file, TH);
-		Vector<VertexSet> c7 = G.All_Cliques(7);
-		printAll(c7);
-		write2file(c7);
+		if(args==null || args.length<4) {
+			help();
+		}
+		else {
+			parse(args);
+			Graph G = new Graph(in_file, TH);
+			//Vector<VertexSet> c5_7 = G.All_Cliques(minQ,maxQ);
+			Vector<VertexSet> c5_7 = G.All_Cliques(maxQ);
+			printAll(c5_7);
+			write2file(c5_7);
+		}
 	}
-	
+	static void help() {
+		System.out.println("Wrong Parameters! should use: java -jar All_Cliques <input file> <round value> <min clique> <max clique> <output file>");
+		System.out.println("Wrong Parameters! should use: java -jar All_Cliques test1.csv 0.7 5 7 test1_out.txt");
+	}
+	static void parse(String[] a){
+		try {
+			in_file = a[0];
+			out_file = a[4];
+			TH = new Double(a[1]);
+			minQ=new Integer(a[2]);
+			maxQ=new Integer(a[3]);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			help();
+		}
+	}
 	
 	static void printAll(Vector<VertexSet> c) {
 		for(int i=0;i<c.size();i++) {
-			System.out.println(i+") "+c.elementAt(i));
+			VertexSet curr = c.elementAt(i);
+			if(curr.size()>=minQ) {
+				System.out.println(i+") "+curr);
+			}
 		}
 	}
 	
 	
 	static void write2file(Vector<VertexSet> c) {
 		FileWriter fw=null;
-		try {fw = new FileWriter("ALL_Cliques_"+in_file+"_"+TH+"_"+Q_size+".txt");} 
+		try {fw = new FileWriter(out_file);} 
 		catch (IOException e) {e.printStackTrace();}
 		PrintWriter os = new PrintWriter(fw);
-		os.println("ALL_Cliques: of file: "+in_file+",  TH:"+TH+" Max Q:"+Q_size);
+		os.println("ALL_Cliques: of file: "+in_file+",  TH:"+TH+" Max Q:"+maxQ);
 		for(int i=0;i<c.size();i++) {
-			os.println(i+") "+c.elementAt(i));
+			VertexSet curr = c.elementAt(i);
+			if(curr.size()>=minQ) os.println(i+") "+curr);
 		}
 		os.close();
 		try {
